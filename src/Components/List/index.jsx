6e73af0ce6dd6abf74e2e71b4ea3287e982fd006
gabrielkamanski0@gmail.com <gabrielkamanski0@gmail.com>
2026-03-30 22:11:38 -0300

    Comit

---
+++ src/Components/List/index.jsx
@@ -0,0 +1,22 @@
+import './style.css'
+function List() {
+    return (
+        <section>
+            <h2 id="exemplo-h2">seção das listas</h2>
+
+            <h3>Lista não ordenada</h3>
+            <ul id="ul">
+                <li>Pão</li>
+                <li>Batata</li>
+                <li>Chocolate</li>
+            </ul>
+            <h3>Lista ordenada</h3>
+            <ul>
+                <li>Hot wheels</li>
+                <li>Digimon</li>
+                <li>POKÉMON</li>
+            </ul>
+        </section>
+    )
+}
+export default List
